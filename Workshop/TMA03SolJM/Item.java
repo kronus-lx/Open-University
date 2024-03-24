379d99c918ff2a1d89520f3e9d006c44dfdb90ac
joelmanning <joelem2@hotmail.com>
2024-03-24 18:57:21 +0000

    added bluej project

---
+++ Workshop/TMA03SolJM/Item.java
@@ -0,0 +1,24 @@
+ 
+
+/**
+ * @brief Interface of an Item class
+*/
+public interface Item {
+    /***
+     * @brief UUID of Item Object
+     * @return
+    */ 
+    public String uuid();
+    
+    /**
+     * @brief Type of Item
+     * @return
+     */
+    public String type();
+    
+    /**
+     * @brief last service of Machine
+     * @return
+     */
+    public int lastServiced();
+}
